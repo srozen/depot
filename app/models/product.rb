9baec4a3884fc54e5acabe9c3c390fc4a3007af0
Samuel Monroe <spat.monroe@gmail.com>
2022-08-16 20:44:07 +0200

    Add products to carts

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ApplicationRecord
+  has_many :line_items
+  before_destroy :ensure_no_references
+
   validates :title, uniqueness: true, length: { minimum: 10 }
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
@@ -6,4 +9,13 @@ class Product < ApplicationRecord
     with: %r{\.(gif|jpg|png)\z}i,
     message: 'must be a URL for GIF, JPG or PNG image.'
   }
+
+  private
+
+  def ensure_no_references
+    unless line_items.empty?
+      errors.add(:base, 'Line items exist for this product')
+      throw :abort
+    end
+  end
 end
