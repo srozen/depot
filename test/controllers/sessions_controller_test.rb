35d3bc3b8b98432a7ac95f43a7cfce6ff4cd99e6
Samuel Monroe <spat.monroe@gmail.com>
2022-08-28 18:20:32 +0200

    Authenticating Users

---
+++ test/controllers/sessions_controller_test.rb
@@ -0,0 +1,25 @@
+require "test_helper"
+
+class SessionsControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get login_url
+    assert_response :success
+  end
+
+  test "should login" do
+    dave = users(:one)
+    post login_url, params: { name: dave.name, password: 'secret' }
+    assert_redirected_to admin_url
+  end
+
+  test "should fail login" do
+    dave = users(:one)
+    post login_url, params: { name: dave.name, password: 'bad' }
+    assert_redirected_to login_url
+  end
+
+  test "should logout" do
+    delete logout_url
+    assert_redirected_to store_index_url
+  end
+end
