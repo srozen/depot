47d1c6f612b1d479b3d81fece85da8dd2d654a2c
Samuel Monroe <spat.monroe@gmail.com>
2022-08-29 11:49:31 +0200

    Limiting access and fixing tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,3 +11,34 @@ class ActiveSupport::TestCase
 
   # Add more helper methods to be used by all tests here...
 end
+
+module AuthenticationHelpers
+  def login_as(user)
+    if respond_to? :visit
+      visit login_url
+      fill_in :name, with: user.name
+      fill_in :password, with: 'secret'
+      click_on 'Login'
+      # Ensures it lets the time to the browser to set session before moving on
+      assert_current_path '/admin'
+    else
+      post login_url, params: { name: user.name, password: 'secret' }
+    end
+  end
+
+  def logout
+    delete logout_url
+  end
+
+  def setup
+    login_as users(:one)
+  end
+end
+
+class ActionDispatch::IntegrationTest
+  include AuthenticationHelpers
+end
+
+class ActionDispatch::SystemTestCase
+  include AuthenticationHelpers
+end
